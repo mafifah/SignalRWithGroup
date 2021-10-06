1954f1e087dfdd5eb42ffe66fbe418e064e6a2f2
M Afifah <mafifah.usr@gmail.com>
2021-10-06 17:20:55 +0700

    Txt

--- SignalRWithGroup/Hubs/ChatHub.cs
+++ SignalRWithGroup/Hubs/ChatHub.cs
@@ -1,48 +1,37 @@
 ﻿using Microsoft.AspNetCore.SignalR;
-using System.Data;
 using System.Threading.Tasks;
 
 namespace SignalRWithGroup.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task OnConnect(string divisi)
+        public async Task MulaiKoneksi(string divisi)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, divisi);
         }
 
-        public async Task SendMessage(ClientMessage clientMessage)
+        public async Task KirimPesan(ClientMessage clientMessage)
         {
-           await Clients.OthersInGroup(clientMessage.Divisi).SendAsync("SendMessage", clientMessage);
+           await Clients.OthersInGroup(clientMessage.Divisi).SendAsync("KirimPesan", clientMessage);
         }
 
-        public async Task SendMessage(string divisi, string namaTabel, DataRow dr)
+        public async Task KirimPesanBroadcast(ClientMessage clientMessage)
         {
-            await Clients.OthersInGroup(divisi).SendAsync("SendMessage", namaTabel,dr);
+            await Clients.Others.SendAsync("KirimPesanBroadcast", clientMessage);
         }
 
-        public async Task SendMessage(string username, string message, string page, byte[] img)
-        {
-            await Clients.All.SendAsync("SendMessage", username, message, img, page);
-        }
-
-        public async Task BroadcastMessage(ClientMessage clientMessage)
-        {
-            await Clients.Others.SendAsync("BroadcastMessage", clientMessage);
-        }
-
-        public async Task OnDisconnect(string divisi)
+        public async Task StopKoneksi(string divisi)
         {
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, divisi);
         }
 
         public class ClientMessage
         {
-            public string Message { get; set; }
             public string Divisi { get; set; }
-            public string Method { get; set; }
-            public long IdKaryawan { get; set; }
-            public string namaPage { get; set; }
+            public string IsiPesan { get; set; }
+            public string JenisPesan { get; set; }
+            public object Id_PrimaryKey { get; set; }
+            public string namaHalaman { get; set; }
         }
     }
 }
