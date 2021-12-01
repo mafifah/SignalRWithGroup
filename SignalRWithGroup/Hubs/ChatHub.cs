5373fb4243e9d947290c984ee585b80a2b6e56d0
Deva Trivanus <devadlsc18@gmail.com>
2021-12-01 16:13:00 +0700

    update terbaru

--- SignalRWithGroup/Hubs/ChatHub.cs
+++ SignalRWithGroup/Hubs/ChatHub.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Data;
 using System.Threading.Tasks;
 
 namespace SignalRWithGroup.Hubs
@@ -7,31 +9,40 @@ namespace SignalRWithGroup.Hubs
     {
         public async Task MulaiKoneksi(string divisi)
         {
-            await Groups.AddToGroupAsync(Context.ConnectionId, divisi);
+            await Groups.AddToGroupAsync(Context.ConnectionId, divisi); //memasukkan id koneksi ke grup
         }
 
-        public async Task KirimPesan(ClientMessage clientMessage)
+        public async Task KirimPesan(PesanSignalR psr)
         {
-           await Clients.OthersInGroup(clientMessage.Divisi).SendAsync("KirimPesan", clientMessage);
+          
+           //await Clients.Client(Context.ConnectionId).SendAsync("KirimPesan",psr); //kirim pesan ke diri sendiri
+            
+           await Clients.OthersInGroup(psr.Divisi).SendAsync("KirimPesan", psr); //kirim pesan ke divisi yang sama
         }
 
-        public async Task KirimPesanBroadcast(ClientMessage clientMessage)
+        public async Task KirimPesanBroadcast(PesanSignalR psr)
         {
-            await Clients.Others.SendAsync("KirimPesanBroadcast", clientMessage);
+            await Clients.Client(Context.ConnectionId).SendAsync("KirimPesanBroadcast",psr); //kirim pesan ke diri sendiri
+            await Clients.All.SendAsync("KirimPesanBroadcast", psr); //kirim pesan broadcast ke semua orang
         }
 
         public async Task StopKoneksi(string divisi)
         {
-            await Groups.RemoveFromGroupAsync(Context.ConnectionId, divisi);
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, divisi); //menhapus id koneksi dari grup
         }
 
-        public class ClientMessage
+        public class PesanSignalR
         {
+            public string Klien { get; set; }
             public string Divisi { get; set; }
+            public object IdUser { get; set; }
+            public long IdForm { get; set; }
+            public string NamaForm { get; set; }
+            public string StatusAction { get; set; }
+            public object PrimaryKey { get; set; }
+            public DateTimeOffset WaktuProses { get; set; }
             public string IsiPesan { get; set; }
             public string JenisPesan { get; set; }
-            public object Id_PrimaryKey { get; set; }
-            public string namaHalaman { get; set; }
         }
     }
 }
